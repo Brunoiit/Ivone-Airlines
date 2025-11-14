7232e52b4b5a58999debca865449824c543757d1
Francisco Javier Castrillon Ruiz <54593236+Brunoiit@users.noreply.github.com>
2025-11-14 17:01:52 -0500

    Quitando detalles

--- airline-front/src/components/Navbar.jsx
+++ airline-front/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          Ivone Airlines
+          Airlines
         </Link>
 
         <button 
